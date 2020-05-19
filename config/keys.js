47abf32ba718fc7e68942ac0a5601f5b61c4f472
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-18 18:09:45 -0700

    electrum register working

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mriesgo:electrum-database-lock@cluster0-ozsip.mongodb.net/test?retryWrites=true&w=majority" 
-  };
+    mongoURI: "mongodb+srv://mriesgo:electrum-database-lock@cluster0-ozsip.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
