9c63b5f3f315c309fe2ffedd27dcf4c692cb0bab
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-18 18:12:34 -0700

    api authorization complete with login and registeer working well

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server working and running on port ${port} !`));
+app.listen(port, () => console.log(`Server running on port ${port} !`));
