313c97fdc68ac994ab2864bbd0fdcb291bba9381
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-24 21:20:30 -0700

    Working on investments section

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const plaid = require("./routes/api/plaid");
 const expenses = require("./routes/api/expenses");
 const budgets = require("./routes/api/budgets");
 const bills = require("./routes/api/bills");
+const investments = require("./routes/api/investments");
 
 const app = express();
 
@@ -49,6 +50,7 @@ app.use("/api/plaid", plaid);
 app.use("/api/expenses", expenses);
 app.use("/api/budgets", budgets);
 app.use("/api/bills", bills);
+app.use("/api/investments", investments);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server running on port ${port} !`));
