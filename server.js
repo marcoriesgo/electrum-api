c48460cf1a150a020b6ca63d85bb3e2fb226fced
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-26 17:18:15 -0700

    updating code for heroku 4

--- server.js
+++ server.js
@@ -41,20 +41,20 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 db.on('open' , ()=>{});
 
 
-// CORS middleware:
-const whitelist = [
-    "*",
-];
-  const corsOptions = {
-    origin: function(origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    }
-  };
-app.use(cors(corsOptions));
+// // CORS middleware:
+// const whitelist = [
+//     "*",
+// ];
+//   const corsOptions = {
+//     origin: function(origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     }
+//   };
+// app.use(cors(corsOptions));
 
 app.use(cors())
 
