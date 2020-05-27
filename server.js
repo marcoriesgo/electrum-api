bf67a04ff1df146c57769ae26353db000f7ab73f
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-26 17:58:46 -0700

    Working on hroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path')
 
 const users = require("./routes/api/users");
 const plaid = require("./routes/api/plaid");
@@ -11,8 +12,6 @@ const bills = require("./routes/api/bills");
 const investments = require("./routes/api/investments");
 
 const app = express();
-
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port
 
 
 // Bodyparser middleware
@@ -24,51 +23,18 @@ app.use(
 app.use(bodyParser.json());
 
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `marco_electrum`;
-
-// Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-
-// Use a simple term for mongoose.connection for accessibility:
-const db = mongoose.connection;
-
-// Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-
-// open the connection to mongo
-db.on('open' , ()=>{});
-
-
-// // CORS middleware:
-// const whitelist = [
-//     "*",
-// ];
-//   const corsOptions = {
-//     origin: function(origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     }
-//   };
-// app.use(cors(corsOptions));
-
-// app.use(cors())
-
 // DB Config
-// const db = require("./config/keys").mongoURI;
+const db = require("./config/keys").mongoURI;
 
-// // Connect to MongoDB
-// mongoose
-//   .connect(
-//     db,
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-//   )
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 
 // Passport middleware
@@ -87,5 +53,16 @@ app.use("/api/budgets", budgets);
 app.use("/api/bills", bills);
 app.use("/api/investments", investments);
 
+//Server static assts if in production:
+if (process.env.NODE_ENV === 'production') { 
+  //set static folder
+  app.use(express.static('client/build'))
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'indx.html'));
+  });
+}
+
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server running on port ${port} !`));
